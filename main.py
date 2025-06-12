9c3102354d051bd7e6b1e78173ac82fbaed0a54c
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-06-12 20:43:59 +0400

    refactor: improve database connection handling in main function
    - Introduced a dedicated DB_PORT variable to enhance clarity and maintainability of the database connection logic.
    - Updated the connection parameters to use the new DB_PORT variable, streamlining the code and improving readability.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
         DB_NAME = os.getenv("DB_NAME")
         DB_USER = os.getenv("DB_USER")
         DB_PASS = os.getenv("DB_PASS")
+        DB_PORT = int(os.getenv("DB_PORT", "5432"))
         TABLE_NAME = os.getenv("TABLE_NAME")
         PUSHGATEWAY_URL = os.getenv("PUSHGATEWAY_URL")
         INSTANCE = os.getenv("INSTANCE")
@@ -34,8 +35,11 @@ def main():
 
         # Connect to DB
         conn = psycopg2.connect(
-            host=DB_HOST, port=int(os.getenv("DB_PORT", "5432")),
-            dbname=DB_NAME, user=DB_USER, password=DB_PASS
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS
         )
         cur = conn.cursor()
 
